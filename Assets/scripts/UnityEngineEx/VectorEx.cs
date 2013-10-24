9967d07d3498286f36ef08e026d215afe1a205e3
Igor Karatayev <igor.cwer@gmail.com>
2013-10-24 18:14:27 +0300

    Vector and Quaternion enumerator lerp and slerp.

--- Assets/scripts/UnityEngineEx/VectorEx.cs
+++ Assets/scripts/UnityEngineEx/VectorEx.cs
@@ -11,6 +11,16 @@ namespace UnityEngineEx
 			return new Vector3(l.x - r.x, l.y - r.y, l.z);
 		}
 
+		public static Vector4 ToPoint(this Vector2 v)
+		{
+			return new Vector4(v.x, v.y, 0, 1.0f);
+		}
+
+		public static Vector4 ToPoint(this Vector3 v)
+		{
+			return new Vector4(v.x, v.y, v.z, 1.0f);
+		}
+
 		public static IEnumerable<Vector3> Circle(this Vector3 v, float Radius, int Sectors)
 		{
 			return v.Circle(Radius, Sectors, 0);
@@ -23,6 +33,46 @@ namespace UnityEngineEx
 				float a = dA0 + i * dA;
 				yield return v + Radius * (new Vector3(Mathf.Cos(a), Mathf.Sin(a), 0));
 			}
+			yield break;
+		}
+
+		public static IEnumerable<Vector2> Lerp(this Vector2 a, Vector2 b, float dT)
+		{
+			float t = 0;
+			while (t < 1) {
+				yield return Vector2.Lerp(a, b, t); t += dT;
+			}
+			yield return Vector2.Lerp(a, b, 1);
+			yield break;
+		}
+
+		public static IEnumerable<Vector3> Lerp(this Vector3 a, Vector3 b, float dT)
+		{
+			float t = 0;
+			while (t < 1) {
+				yield return Vector3.Lerp(a, b, t); t += dT;
+			}
+			yield return Vector3.Lerp(a, b, 1);
+			yield break;
+		}
+
+		public static IEnumerable<Vector4> Lerp(this Vector4 a, Vector4 b, float dT)
+		{
+			float t = 0;
+			while (t < 1) {
+				yield return Vector4.Lerp(a, b, t); t += dT;
+			}
+			yield return Vector4.Lerp(a, b, 1);
+			yield break;
+		}
+
+		public static IEnumerable<Vector3> Slerp(this Vector3 a, Vector3 b, float dT)
+		{
+			float t = 0;
+			while (t < 1) {
+				yield return Vector3.Slerp(a, b, t); t += dT;
+			}
+			yield return Vector3.Slerp(a, b, 1);
 			yield break;
 		}
 	}
