91840f644adee16b73aab765ca9700bab11dd2a5
Igor Karatayev <igor.cwer@gmail.com>
2013-11-20 14:55:01 +0300

    MathfEx - Repeat for negative values.

---
+++ Assets/scripts/UnityEngineEx/MathfEx.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+namespace UnityEngineEx
+{
+	public static class MathfEx
+	{
+		public static int Repeat(int t, int length)
+		{
+			while (t < 0)
+				t += length;
+
+			return t % length;
+		}
+	}
+}
