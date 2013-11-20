17e1031e32cbc76974f8f3bebf1e2dd33ff3141b
Igor Karatayev <igor.cwer@gmail.com>
2013-11-20 16:33:00 +0300

    Texture manipulation and TextAnchor.

---
+++ Assets/scripts/UnityEngineEx/TextAnchorEx.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngineEx;
+using System;
+
+
+namespace MagicDice
+{
+	public static class TextAnchorEx
+	{
+		public static Vector2 ToPivot(this TextAnchor anchor)
+		{
+			float pivotx, pivoty;
+			if (anchor < TextAnchor.MiddleLeft)
+				pivoty = 1.0f;
+			else if (anchor < TextAnchor.LowerLeft)
+				pivoty = 0.5f;
+			else
+				pivoty = 0.0f;
+
+			int a = (int)(anchor) % 3;
+			if (a == 0)
+				pivotx = 0.0f;
+			else if (a == 1)
+				pivotx = 0.5f;
+			else
+				pivotx = 1.0f;
+
+			return new Vector2(pivotx, pivoty);
+		}
+	}
+}
--- Assets/scripts/UnityEngineEx/TextureEx.cs
+++ Assets/scripts/UnityEngineEx/TextureEx.cs
@@ -15,6 +15,48 @@ namespace UnityEngineEx
 			return new Rect(0, 0, texture.width, texture.height);
 		}
 
+		public static Texture2D Trim(this Texture2D texture)
+		{
+			Color[] c = texture.GetPixels();
+			int minx = texture.width;
+			int miny = texture.height;
+			int maxx = 0;
+			int maxy = 0;
+
+			for (int y = 0; y < texture.height; y++)
+				for (int x = 0; x < texture.width; x++) {
+					Color cc = c[x + y * texture.width];
+					if (cc.a > 0) {
+						if (minx > x) minx = Mathf.Min(x - 1, minx);
+						if (miny > y) miny = Mathf.Min(y - 1, miny);
+						if (maxx < x) maxx = Mathf.Max(x + 1, maxx);
+						if (maxy < y) maxy = Mathf.Max(y + 1, maxy);
+					}
+				}
+
+			maxx++; maxy++;
+
+			if (minx < 0) minx = 0;
+			if (miny < 0) miny = 0;
+			if (maxx > texture.width) maxx = texture.width;
+			if (maxy > texture.height) maxx = texture.height;
+
+			int dx = maxx - minx;
+			int dy = maxy - miny;
+			Color[] nc = new Color[dx * dy];
+			for (int y = 0; y < dy; y++)
+				for (int x = 0; x < dx; x++) {
+					nc[x + y * dx] = c[minx + x + (miny + y) * texture.width];
+				}
+
+			Texture2D newtexture = new Texture2D(dx, dy, texture.format, false);
+			newtexture.filterMode = texture.filterMode;
+			newtexture.wrapMode = texture.wrapMode;
+			newtexture.SetPixels(nc);
+			newtexture.Apply();
+			return newtexture;
+		}
+
 		public static Texture2D MakeOutline(this Texture2D texture, int width)
 		{
 			Color[] c = texture.GetPixels();
@@ -24,12 +66,14 @@ namespace UnityEngineEx
 				for (int x = 0; x < texture.width; x++) {
 					Color cc = c[x + y * texture.width];
 					if (cc.a < 0.1) {
-						for (int cy = Mathf.Max(y - width, 0); cy < Mathf.Min(y + (width + 1), texture.height - 1); cy++)
-							for (int cx = Mathf.Max(x - width, 0); cx < Mathf.Min(x + (width + 1), texture.width - 1); cx++)
+						for (int cy = Mathf.Max(y - width, 0); cy <= Mathf.Min(y + width, texture.height - 1); cy++)
+							for (int cx = Mathf.Max(x - width, 0); cx <= Mathf.Min(x + width, texture.width - 1); cx++) {
+								cc = c[cx + cy * texture.width];
 								if (cc.a > 0.1) {
 									nc[x + y * texture.width] = Color.black;
 									goto skip;
 								}
+							}
 					}
 					if (cc.a > 0) {
 						cc = cc.Alpha(1.0f);
@@ -38,7 +82,7 @@ namespace UnityEngineEx
 				skip:;
 				}
 
-			texture.SetPixels(nc);
+			texture.SetPixels(nc, 0);
 			texture.Apply();
 			return texture;
 		}
