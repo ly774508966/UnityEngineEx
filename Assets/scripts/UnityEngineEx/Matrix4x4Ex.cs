6d2e28b3e93dc940954c8dfb5d18bef52ea47d88
Igor Karatayev <igor.cwer@gmail.com>
2014-02-03 19:09:40 +0300

    Matrix4x4 ops.

---
+++ Assets/scripts/UnityEngineEx/Matrix4x4Ex.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace UnityEngineEx
+{
+	public static class Matrix4x4Ex
+	{
+		public static Matrix4x4 Column(this Matrix4x4 m4x4, int column, Vector3 v)
+		{
+			m4x4.SetColumn(column, v);
+			return m4x4;
+		}
+
+		public static Matrix4x4 Columns(this Matrix4x4 m4x4, params Vector3[] vs)
+		{
+			int i = 0;
+			foreach (var v in vs) {
+				m4x4.SetColumn(i++, v);
+			}
+			return m4x4;
+		}
+
+		public static Matrix4x4 Columns(this Matrix4x4 m4x4, params Vector4[] vs)
+		{
+			int i = 0;
+			foreach (var v in vs) {
+				m4x4.SetColumn(i++, v);
+			}
+			return m4x4;
+		}
+	}
+}
