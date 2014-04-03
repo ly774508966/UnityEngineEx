ba791c1b5ec9b99d33b40a2ecc077482b66e7326
Igor Karatayev <igor.cwer@gmail.com>
2014-04-03 14:51:01 +0300

    Repeat for negative floats.

--- Assets/Plugins/UnityEngineEx/MathfEx.cs
+++ Assets/Plugins/UnityEngineEx/MathfEx.cs
@@ -18,6 +18,20 @@ namespace UnityEngineEx
 			return t % length;
 		}
 
+		/// <summary>
+		/// Repeat value in range [0..length] similar to mod operator. Works for negative values.
+		/// </summary>
+		/// <param name="t"></param>
+		/// <param name="length"></param>
+		/// <returns></returns>
+		public static float Repeat(float t, float length)
+		{
+			while (t < 0)
+				t += length;
+
+			return Mathf.Repeat(t, length);
+		}
+
 		public static float Cbrt(float v)
 		{
 			if (v >= 0) {
