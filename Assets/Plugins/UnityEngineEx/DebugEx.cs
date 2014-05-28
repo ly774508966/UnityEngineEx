cf6b2ccefc337798ae9063ff89eb97495e422634
Igor Karatayev <igor.cwer@gmail.com>
2014-05-29 01:57:30 +0300

    Debug.Log system ILogger

---
+++ Assets/Plugins/UnityEngineEx/DebugEx.cs
@@ -0,0 +1,23 @@
+﻿using SystemEx;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace UnityEngineEx
+{
+    public static class DebugEx
+    {
+        public static void Log(string message, params object[] args)
+        {
+            Debug.Log(string.Format(message, args));
+        }
+    }
+
+    public class Logger : ILogger
+    {
+        public void Log(string message, params object[] args)
+        {
+            Debug.Log(string.Format(message, args));
+        }
+    }
+}
