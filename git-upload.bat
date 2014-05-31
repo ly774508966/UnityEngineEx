32a5096954d10c20f588f078050883268214e4e2
Igor Karatayev <igor.cwer@gmail.com>
2014-05-31 21:49:42 +0300

    submodule update

---
+++ git-upload.bat
@@ -0,0 +1,13 @@
+cd .\Libraries\SystemEx
+	git checkout master
+	git pull origin master
+cd ..\..
+
+cd .\Libraries\MathEx\ 
+	git checkout master
+	git pull origin master
+cd ..\..
+
+git add *
+git commit
+git push
