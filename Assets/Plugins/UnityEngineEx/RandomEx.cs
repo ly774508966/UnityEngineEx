aeb05d7d2ebf903b07f9ed261d83741c39ea03ce
Igor Karatayev <igor.cwer@gmail.com>
2014-08-26 00:59:35 +0300

    RandomEx and Unity watch provider. Removed StateMachine.

---
+++ Assets/Plugins/UnityEngineEx/RandomEx.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UnityEngineEx
+{
+	public static class RandomEx
+	{
+		public static float RandomDiff(this float v)
+		{
+			return v + Random.Range(-0.1f * v, 0.1f * v);
+		}
+	}
+}
---
+++ Assets/Plugins/UnityEngineEx/Watch.cs
@@ -0,0 +1,25 @@
+﻿using SystemEx;
+using UnityEngine;
+
+namespace UnityEngineEx
+{
+	class Watch
+	{
+	}
+
+	public class UnityClockProvider : IClockProvider
+	{
+		public float tick { get { return getTick(); } }
+
+
+
+		public UnityClockProvider()
+		{
+		}
+
+		float getTick()
+		{
+			return Time.time;
+		}
+	}
+}
