b659d418280828dc82fd4018df8681758aa4e00a
Igor Karatayev <igor.cwer@gmail.com>
2014-09-01 16:53:53 +0300

    Changed MonoBehaviourEx function search to attribute based. Not name based. Added Type description caching.

---
+++ Assets/Plugins/UnityEngineEx/Attributes/BehaviourFunctionAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnityEngineEx
+{
+	public class BehaviourFunctionAttribute : Attribute
+	{
+		public string name;
+
+		public BehaviourFunctionAttribute(string name)
+		{
+			this.name = name;
+		}
+	}
+}
--- Assets/Plugins/UnityEngineEx/MonoBehaviourEx.cs
+++ Assets/Plugins/UnityEngineEx/MonoBehaviourEx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using SystemEx;
 using UnityEngine;
 
@@ -8,11 +9,29 @@ namespace UnityEngineEx
 {
 	public class MonoBehaviourEx : MonoBehaviour
 	{
+		class MonoBehaviourDescription 
+		{
+			public List<MethodInfo> awake = new List<MethodInfo>(); 
+		}
+
+		static Dictionary<Type, MonoBehaviourDescription> descriptions = new Dictionary<Type,MonoBehaviourDescription>();
+
 		protected virtual void Awake()
 		{
 			this.Dissolve();
 
-			foreach (var m in this.GetType().GetMethods("Awake_")) {
+			MonoBehaviourDescription mbd;
+			if (!descriptions.TryGetValue(this.GetType(), out mbd)) {
+				mbd = new MonoBehaviourDescription();
+				foreach (var m in this.GetType().GetMethodsAndAttributes<BehaviourFunctionAttribute>()) {
+					if (m.Item2.name == "Awake")
+						mbd.awake.Add(m.Item1);
+				}
+
+				descriptions.Add(this.GetType(), mbd);
+			}
+
+			foreach (var m in mbd.awake) {
 				var mp = m.GetParameters();
 				if (mp.Length > 0) {
 					var pl = new List<object>(mp.Length);
