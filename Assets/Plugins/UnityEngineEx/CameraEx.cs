b74227ca282605de189e5819ed1596a891cc9db1
Igor Karatayev <igor.cwer@gmail.com>
2014-10-02 09:23:22 +0300

    Camera extensions.

---
+++ Assets/Plugins/UnityEngineEx/CameraEx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace UnityEngineEx
+{
+	public static class CameraEx
+	{
+		public static Vector2 GetPixelSize(this Camera c)
+		{
+			return new Vector2(c.pixelWidth, c.pixelHeight);
+		}
+	}
+}
