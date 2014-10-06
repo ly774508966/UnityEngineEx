a031788d177164ec30b1aaeac0a7b324e4422681
Igor Karatayev <igor.cwer@gmail.com>
2014-10-06 15:48:07 +0300

    Resource paths.

--- Assets/Plugins/UnityEngineEx/ResourcesEx.cs
+++ Assets/Plugins/UnityEngineEx/ResourcesEx.cs
@@ -1,9 +1,34 @@
+using System;
+using System.IO;
+using System.Reflection;
 using UnityEngine;
 
 namespace UnityEngineEx
 {
 	public static class ResourcesEx
 	{
+		public static readonly string projectPath;
+		public static readonly string resourcePath;
+
+		static ResourcesEx()
+		{
+			string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+			UriBuilder uri = new UriBuilder(codeBase);
+			string path = Uri.UnescapeDataString(uri.Path);
+			projectPath = Path.GetFullPath(new Uri(Path.Combine(Path.GetDirectoryName(path), "../..")).AbsolutePath);
+			resourcePath = Path.GetFullPath(new Uri(Path.Combine(Path.GetDirectoryName(path), "../../Assets/Resources")).AbsolutePath);
+		}
+
+		public static string GetAssetPath(string path)
+		{
+			var p = Path.GetFullPath(path);
+			if (p.StartsWith(projectPath)) {
+				return p.Substring(projectPath.Length + 1);
+			}
+
+			return null;
+		}
+
 		public static T Load<T>(string path) where T : class
 		{
 			if (path == ".color") {
@@ -12,8 +37,9 @@ namespace UnityEngineEx
 			return Resources.Load(path, typeof(T)) as T;
 		}
 
-		static Material color_;
-		static T LoadColor<T>() where T : class
+		private static Material color_;
+
+		private static T LoadColor<T>() where T : class
 		{
 			if (typeof(T) == typeof(Material)) {
 				if (color_ == null) {
@@ -25,4 +51,3 @@ namespace UnityEngineEx
 		}
 	}
 }
-
